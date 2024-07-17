4858129b7ebaf889ac8b1cd4c646700b9282fb3d
neerajan rai <141233559+SayianPi@users.noreply.github.com>
2024-07-17 21:38:39 +0545

    completed - displaying data and handling events

--- ProjectAngular/src/app/app.module.ts
+++ ProjectAngular/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';              // for two-way binding
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponents } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
+import { SummaryPipe } from './summary.pipe';              // for custom pipe
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponents,
-    CourseComponent
+    CourseComponent,
+    SummaryPipe          // custom pipe     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule          // Import FormsModule for two-way data binding
   ],
   providers: [
     CoursesService
--- ProjectAngular/src/app/courses.component.ts
+++ ProjectAngular/src/app/courses.component.ts
@@ -36,6 +36,23 @@ import { CoursesService } from "./courses.service";
                  <div (click)="onDivClick()">
                     <button (click)="onBubbleClick($event)">Bubble</button>
                  </div>
+                <!--event filtering-->
+                 <input (keyup) = "onKeyUp($event)"/>
+                 <input (keyup.enter)="onKey()"/> 
+                 <!--template variable-->
+                 <input (keyup.enter)="enter($event)" />
+                 <input #greet (keyup.enter)="method(greet.value)"/>
+                 <!--two-way binding-->
+                 <input [(ngModel)]="email" (keyup.enter)="onKeys()"/> <br>
+                 <!--Pipes-->
+                 {{ book.title | uppercase}} <br>
+                 {{ book.price | currency:'AUD'}} <br>
+                 {{ book.author | uppercase}} <br>
+                 {{ book.rating |number:'1.1-1'}} <br>
+                 {{ book.sold |number}} <br>
+                 {{ book.relaseDate |date:'shortDate'}} <br>
+                 <!--custom pipe-->
+                 {{ text | summary:20 }}
                 `
 })
 export class CoursesComponents{
@@ -45,6 +62,7 @@ export class CoursesComponents{
     imageUrl = "https://media.istockphoto.com/id/147290529/photo/emperors.jpg?s=612x612&w=0&k=20&c=ZApZFJtKoXGKYYJsgNcNPTMHqqSbbAx9CBg2AF2qyJk=";
     colSpan = 2;
     isActive = true;
+    email = "me@example.com";
     // getDescription(){
     //     return this.title + " are: "; 
     // }
@@ -77,4 +95,29 @@ export class CoursesComponents{
         $event.stopPropagation();
         console.log("Event Bubbling");
     }
+    onKeyUp($event){
+        if ($event.keyCode === 13)
+            console.log("enter was clicked.")
+    }
+    onKey(){
+        console.log("enter was pressed.");
+    }
+    enter($event){
+        console.log($event.target.value);
+    }
+    method(greet){
+        console.log(greet);
+    }
+    onKeys(){
+        console.log(this.email);
+    }
+    book = {
+        title: "How to Code like a Pro",
+        rating: 4.899,
+        sold: 19082,
+        author: "Neerajan Rai",
+        price: 400.99,
+        relaseDate: new Date(2016,3,1)
+    }
+    text = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
 }
