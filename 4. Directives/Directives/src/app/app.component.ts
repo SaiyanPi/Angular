8371a21781ccecae0d16fa7b8789552fb89803d6
neerajan rai <141233559+SayianPi@users.noreply.github.com>
2024-07-27 15:14:28 +0545

    added - directives upto 'ngFor and change detection'

--- 4. Directives/Directives/src/app/app.component.ts
+++ 4. Directives/Directives/src/app/app.component.ts
@@ -9,6 +9,34 @@ export class AppComponent {
   maintitle = 'Directives';
   titleOne = 'ngIf';
   titleTwo = 'hidden property';
+  titleThree = 'ngSwitchCase';
+  titleFour = 'ngFor';
+  titleFive = 'ngFor and change detection';
 
+  
+  // ngIf
+  // hidden property
   courses = [1,2];
+
+  // ngSwitchCase
+  viewMode = 'map'; //property initialized
+
+  // ngFor
+  books = [
+    {id: 1, name: 'Atomic Habits'},
+    {id: 2, name: 'Sunconscious Mind'},
+    {id: 3, name: 'Into The Wild'},
+  ];
+
+  // ngFor and change detection
+  addBook(){
+    this.books.push({id: 4, name: 'Infulence People'});
+  }
+  removeBook(book: any) {
+    let index = this.books.indexOf(book);
+    this.books.splice(index, 1);
+  }
+  editBook(book: any) {
+    book.name = 'Updated Book';
+  }
 }
